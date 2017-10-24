e761e6cf907e11b50a6bff16b139c55bee6c3845
poisson0106 <poisson0106@gmail.com>
2017-10-24 16:08:10 +0800

    index ongoing

---
+++ src/components/content/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data: function () {
+    return {
+      rproducts : []
+    }
+  },
+  beforeCreate: function () {
+    var self = this
+    this.axios({
+      method: "get",
+      url: 'http://localhost:8080/product/recommended'
+    })
+    .then(function (response) {
+      self.rproducts = response.data
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
